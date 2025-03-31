bd8b927cfa573a6344a1b47117e3655f8393efdf
Juanmanms <juanmanms@gmail.com>
2025-03-31 16:14:09 +0200

    log YouTube API key during application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('YOUTUBE_API_KEY:', process.env.YOUTUBE_API_KEY);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
