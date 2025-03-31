e4cdfbc5b22ae797de89e384cb1ab1193dffac9d
Juanmanms <juanmanms@gmail.com>
2025-03-31 13:56:32 +0200

    remove environment variable checker script

--- src/check-env.js
+++
@@ -1,7 +0,0 @@
-console.log('Environment Variables:');
-console.log('-----------------------');
-console.log('YOUTUBE_API_KEY:', process.env.YOUTUBE_API_KEY || 'Not Set');
-console.log('YOUTUBE_CHANNEL_ID:', process.env.YOUTUBE_CHANNEL_ID || 'Not Set');
-console.log('TELEGRAM_BOT_TOKEN:', process.env.TELEGRAM_BOT_TOKEN || 'Not Set');
-console.log('TELEGRAM_CHAT_ID:', process.env.TELEGRAM_CHAT_ID || 'Not Set');
-console.log('CHECK_INTERVAL:', process.env.CHECK_INTERVAL || 'Not Set');
