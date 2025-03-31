fd41658dd89a40b1b398338ac81e1c5a033620c5
Juanmanms <juanmanms@gmail.com>
2025-03-31 15:57:21 +0200

    add new video ID, update config module for environment variable validation, and change TypeScript target to ES2021

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import configuration from './configuration';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
+    NestConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env', // Asegúrate de que apunte al archivo correcto
+      load: [configuration],
+      validationSchema: Joi.object({
+        YOUTUBE_API_KEY: Joi.string().required(),
+        YOUTUBE_CHANNEL_ID: Joi.string().required(),
+        TELEGRAM_BOT_TOKEN: Joi.string().required(),
+        TELEGRAM_CHAT_ID: Joi.string().required(),
+        CHECK_INTERVAL: Joi.number().default(5),
+      }),
+      validationOptions: {
+        abortEarly: true,
+      },
     }),
   ],
 })
-export class AppModule {}
+export class ConfigModule {}
